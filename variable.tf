0c94248e3e0b79fc315e9e9e7cf857c68125022f
Pratik <pratikzende882002@gmail.com>
2026-04-21 12:00:31 +0530

    Remove sg_name variable from variable.tf
    Removed sg_name variable definition from variable.tf.

--- variable.tf
+++ variable.tf
@@ -26,11 +26,6 @@ variable "instance_name" {
   
 }
 
-variable "sg_name" {
-    description = "The name of the security group."
-    type        = string
-  
-}
 
 variable "vpc_id" {
     description = "The ID of the VPC where the security group will be created."
